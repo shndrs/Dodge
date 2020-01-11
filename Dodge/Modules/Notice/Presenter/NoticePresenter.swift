0564f190bde86b472c16b23b49598bad84fad924
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-01-11 11:05:45 +0330

    Added Notice Presenter and Protocols

---
+++ Dodge/Modules/Notice/Presenter/NoticePresenter.swift
@@ -0,0 +1,45 @@
+//
+//  NoticePresenter.swift
+//  Dodge
+//
+//  Created by NP2 on 1/11/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import UIKit
+
+class NoticePresenter: NoticePresenterProtocol {
+    
+    var view: PresenterToViewProtocol?
+    var interactor: PresenterToInteractorProtocol?
+    var router: NoticeRouterProtocol?
+    
+}
+
+// MARK: - Methods
+
+extension NoticePresenter {
+    
+    func startFetchingNotice() {
+        interactor?.fetchNotice()
+    }
+       
+    func showMovieController(navigationController: UINavigationController) {
+        router?.pushToMovieScreen(navigationConroller: navigationController)
+    }
+    
+}
+
+// MARK: - Interactor Implementation
+
+extension NoticePresenter: InteractorToPresenterProtocol {
+    
+    func noticeFetchedSuccess(noticeModelArray: [NoticeModel]) {
+        view?.showNotice(noticeArray: noticeModelArray)
+    }
+    
+    func noticeFetchFailed() {
+        view?.showError()
+    }
+    
+}
---
+++ Dodge/Modules/Notice/Protocols/NoticeProtocols.swift
@@ -0,0 +1,43 @@
+//
+//  NoticeProtocols.swift
+//  Dodge
+//
+//  Created by NP2 on 1/11/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol NoticePresenterProtocol: class {
+    
+    var view: PresenterToViewProtocol? { get set }
+    var interactor: PresenterToInteractorProtocol? { get set }
+    var router: NoticeRouterProtocol? { get set }
+    func startFetchingNotice()
+    func showMovieController(navigationController: UINavigationController)
+}
+
+protocol PresenterToViewProtocol: class {
+    
+    func showNotice(noticeArray:Array<NoticeModel>)
+    func showError()
+}
+
+protocol NoticeRouterProtocol: class {
+    
+    static func createModule() -> NoticeVC
+    func pushToMovieScreen(navigationConroller: UINavigationController)
+}
+
+protocol PresenterToInteractorProtocol: class {
+    
+    var presenter:InteractorToPresenterProtocol? { get set }
+    func fetchNotice()
+}
+
+protocol InteractorToPresenterProtocol: class {
+    
+    func noticeFetchedSuccess(noticeModelArray: [NoticeModel])
+    func noticeFetchFailed()
+}
