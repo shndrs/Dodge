7c07b17b5038674e9827eeb4bb772d384d34f8dd
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-01-11 12:13:45 +0330

    Added RIDs and ReusableView

---
+++ Dodge/Utills/RIDs.swift
@@ -0,0 +1,16 @@
+//
+//  RIDs.swift
+//  Dodge
+//
+//  Created by NP2 on 1/11/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+struct RIDs {
+    
+    static let noticeVC = [
+        NoticeCell.reuseIdentifier
+    ]
+    
+}
+
---
+++ Dodge/Utills/ReusableView.swift
@@ -0,0 +1,19 @@
+//
+//  ReusableView.swift
+//  Dodge
+//
+//  Created by NP2 on 1/11/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView {}
+
+extension ReusableView where Self: UIView {
+
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
