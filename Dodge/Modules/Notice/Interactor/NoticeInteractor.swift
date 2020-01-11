084e71e2101a1d384e90e91dc26b931a3654aa03
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-01-11 11:19:07 +0330

    Added NoticeInteractor

---
+++ Dodge/Modules/Notice/Interactor/NoticeInteractor.swift
@@ -0,0 +1,40 @@
+//
+//  NoticeInteractor.swift
+//  Dodge
+//
+//  Created by NP2 on 1/11/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import Alamofire
+import ObjectMapper
+
+final class NoticeInteractor: PresenterToInteractorProtocol {
+    
+    var presenter: InteractorToPresenterProtocol?
+    
+}
+
+// MARK: - Methods
+
+extension NoticeInteractor {
+    
+    func fetchNotice() {
+        Alamofire.request(Routes.noticeList).responseJSON { [weak self] response in
+            
+            guard self != nil else { return }
+            
+            if(response.response?.statusCode == 200) {
+                
+                if let json = response.result.value as AnyObject? {
+                    let arrayResponse = json["notice_list"] as! NSArray
+                    let arrayObject = Mapper<NoticeModel>().mapArray(JSONArray: arrayResponse as! [[String : Any]]);
+                    self?.presenter?.noticeFetchedSuccess(noticeModelArray: arrayObject)
+                }
+            } else {
+                self?.presenter?.noticeFetchFailed()
+            }
+        }
+    }
+    
+}
